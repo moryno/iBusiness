685776a92e85cb23bbd045756b2e681f7435157b
jaykaranja <mainakaranja2001@gmail.com>
2023-03-13 17:04:11 +0300

    Prepping data for sending

--- src/components/PurchaseOrder/Form.js
+++ src/components/PurchaseOrder/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../../assets/P_order.css';
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
@@ -20,100 +20,83 @@ const dateString = `${year}-${month}-${day}`;
 
 // Form Component
 
-export const Form = () => {
-    const [costCenter, setCostCenter] = useState(1);
-    const [supplier, setSupplier] = useState(1);
-    const [shipsTo, setShipsTo] = useState(1);
+const Form = ({ setSubmitData }) => {
+    const [costCenter, setCostCenter] = useState("1");
+    const [supplier, setSupplier] = useState("1");
+    const [shipsTo, setShipsTo] = useState("1");
     const [orderDate, setOrderDate] = useState(dateString);
+    const [orderAmount, setOrderAmount] = useState("");    
     const [firstDeliveryDate, setfirstDeliveryDate] = useState(dateString);
     const [narration, setNarration] = useState('');
-    const [deliveryPeriod, setDeliveryPeriod] = useState(0);
+    const [deliveryPeriod, setDeliveryPeriod] = useState("");
     const [driverDetails, setdriverDetails] = useState('');
-  
-      // Handles change of values in input fields
-    
-      const handleDeliveryPeriod = (e) => {
-         setDeliveryPeriod(e.value);
-      }
-  
-      const handleNarration = (e) => {
-         setNarration(e.value);
-      }
-  
-      const handleDriverDetails = (e) => {
-         setdriverDetails(e.value);
-  
-      }
-  
-      const handleOrderDate = (e) => {
-         setOrderDate(e.value);
-      }
-  
-      const handleFirstdDeliveryDate = (e) => {
-        setfirstDeliveryDate(e.value);
-      }
-      
-  
-      const handleCostCenter = (e) => {
-        setCostCenter(e.value);
 
-      }
-      
-      const handleSupplier = (e) => {
-        setSupplier(e.value);
-      }
-  
-      const handleShipsTo = (e) => {
-        setShipsTo(e.value);
-      }
-  
-  
-      // End of function
+    useEffect(
+      () => {
+        setSubmitData(
+          [{
+            costCenter: costCenter,
+            supplier: supplier,
+            shipsTo: shipsTo,
+            orderDate: orderDate,
+            orderAmount: orderAmount,
+            deliveryPeriod: deliveryPeriod,
+            firstDeliveryDate: firstDeliveryDate,
+            driverDetails: driverDetails,
+            narration: narration
+          }
+          ]
+        )
+      }, [costCenter, supplier, shipsTo, orderDate, orderAmount, firstDeliveryDate, narration, deliveryPeriod, driverDetails]
+    )
+
   
     return (
         <div className='po-form'>
           <div className='po-left'>
             <div className='select-control'>
               <label className='label-control'>Cost Center:</label>
-              <SelectBox items={centerOptions} searchEnabled={true} valueExpr="value" displayExpr="text" width="68%" height="4.5vh" value={costCenter} onValueChanged={handleCostCenter}/>
+              <SelectBox items={centerOptions} searchEnabled={true} valueExpr="text" displayExpr="text" width="68%" height="4.5vh" value={costCenter} onValueChanged={(e) => {setCostCenter(e.value)}}/>
             </div>
             <div className='select-control'>
               <label className='label-control'>Ships to:</label>
-              <SelectBox items={centerOptions} valueExpr="value" displayExpr="text" width="68%" height="4.5vh" value={shipsTo} onValueChanged={handleShipsTo}/>
+              <SelectBox items={centerOptions} valueExpr="text" displayExpr="text" width="68%" height="4.5vh" value={shipsTo} onValueChanged={(e) => {setShipsTo(e.value)}}/>
             </div>
             <div className='select-control'>
               <label className='label-control'>Order Amount:</label>
-              <TextBox width="68%" height="4vh"/>
+              <TextBox width="68%" height="4vh" value={orderAmount} onValueChanged={(e) => {setOrderAmount(e.value)}} />
             </div>
             <div className='select-control'>
               <label className='label-control'>First Delivery Date:</label>
-              <DateBox defaultValue={dateString} width="68%" height="4vh" value={firstDeliveryDate} onValueChanged={handleFirstdDeliveryDate} />
+              <DateBox defaultValue={dateString} width="68%" height="4vh" value={firstDeliveryDate} onValueChanged={(e) => {setfirstDeliveryDate(e.value)}} />
             </div>
           </div>
           <div className='po-right'>
             <div className='select-control'>
               <label className='label-control'>Supplier:</label>
-              <SelectBox items={centerOptions} valueExpr="value" displayExpr="text" width="68%" height="4.5vh" value={supplier} onValueChanged={handleSupplier}/>
+              <SelectBox items={centerOptions} valueExpr="text" displayExpr="text" width="68%" height="4.5vh" value={supplier} onValueChanged={(e) => {setSupplier(e.value)}}/>
             </div>
             <div className='select-control'>
               <label className='label-control'>Order Date:</label>
-              <DateBox defaultValue={dateString} width="68%" height="4vh" value={orderDate} onValueChanged={handleOrderDate} />
+              <DateBox defaultValue={dateString} width="68%" height="4vh" value={orderDate} onValueChanged={(e) => {setOrderDate(e.value)}} />
             </div>
             <div className='select-control'>
               <label className='label-control'>Delivery Period (Days):</label>
-              <TextBox width="79%" height="4vh" value={deliveryPeriod} onValueChanged={handleDeliveryPeriod} />
+              <TextBox width="79%" height="4vh" value={deliveryPeriod} onValueChanged={(e) => {setDeliveryPeriod(e.value)}} />
             </div>
             <div className='select-control'>
               <label className='label-control'>Vehicle/Driver Details:</label>
-              <TextBox width="79%" height="4vh" value={driverDetails} onValueChanged={handleDriverDetails} />
+              <TextBox width="79%" height="4vh" value={driverDetails} onValueChanged={(e) => {setdriverDetails(e.value)}} />
             </div>
           </div>
           <div className='select-control'>
             <label className='label-control'>Narration:</label>
-            <TextArea type='text' width="40vw" height="9vh" placeholder="Type narration here" style={{ marginLeft: "3.3rem"}}className="select-control-input" value={narration} onValueChanged={handleNarration} />
+            <TextArea type='text' width="40vw" height="9vh" placeholder="Type narration here" style={{ marginLeft: "3.3rem"}} className="select-control-input" value={narration} onValueChanged={(e) => {setNarration(e.value)}} />
           </div>
         </div>
     )
   }
   
   // End of component code
+
+  export default Form;
