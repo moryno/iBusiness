1520c2aa2d1258b2efdbdafd06b999c15a860335
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2023-03-14 10:41:34 +0300

    Persist user when logged in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import { useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import Home from "./pages/Home";
@@ -12,38 +9,20 @@ import Profile from "./pages/Profile";
 import { PurchaseOrder } from "./pages/PurchaseOrder";
 
 import Layout from "./components/Layout";
-import { setupLogin } from "./helpers/auth";
-import { loginSuccess } from "./redux/userSlice";
-import request from "./helpers/requestMethod";
 
 function App() {
-  const location = useLocation();
-  const dispatch = useDispatch();
-  // const token = hash.split("=")[1];
   const currentUser = useSelector((state) => state.user?.currentUser?.user);
-  console.log(location);
-
-  // useEffect(() => {
-  //   const getUser = async () => {
-  //     const { data } = await request.get("/User");
-  //     console.log(data);
-  //     setupLogin(data?.token);
-  //     dispatch(loginSuccess(data));
-  //   };
-
-  //   if (token) getUser();
-  // }, [token, dispatch]);
 
   const ProtectedRoute = ({ children }) => {
-    if (!currentUser) {
-      // return (window.location.href =
-      //   "https://i-business-ui-git-main-moryno.vercel.app/");
-      console.log(currentUser);
-    }
+    // if (!currentUser) {
+    //   return (window.location.href =
+    //     "https://i-business-ui-git-main-moryno.vercel.app/");
+    // }
 
     return children;
   };
 
+  console.log(useSelector((state) => state.user));
   const router = createBrowserRouter([
     {
       path: "/",
