efbf7710f6276a8178f097d15f21a9c5b8231444
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2023-03-14 08:04:13 +0300

    Modify the redirect url when user sign out

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,5 +1,6 @@
 export const logoutFunc = () => {
   localStorage.removeItem("token");
+  window.location.href = "https://i-business-ui.vercel.app/";
 };
 
 export const setupLogin = (token) => {
--- src/helpers/requestMethod.js
+++ src/helpers/requestMethod.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  "http://192.168.1.200:https://bookingappmicroservice.azurewebsites.net/api/";
+const BASE_URL = "https://bookingappmicroservice.azurewebsites.net/api";
 
 let request = axios.create({ baseURL: BASE_URL });
 
@@ -23,5 +22,5 @@ request.interceptors.request.use(
 export default request;
 
 export const msSingleSign =
-  "http://192.168.1.200:7030/login/Loginwithmicrosoft";
+  "https://bookingapptrial.azurewebsites.net/login/Loginwithmicrosoft";
 // "https://login.microsoftonline.com/429eb2c8-ad5d-4e03-b326-a26d27a067f7/oauth2/authorize?client_id=fd6d9002-4eb4-4b56-b3a4-29f9cf05141f&response_type=token&redirect_uri=http://localhost:3000/login&resource=fd6d9002-4eb4-4b56-b3a4-29f9cf05141f&scope=openid&response_mode=fragment&state=12345&nonce=678910";
