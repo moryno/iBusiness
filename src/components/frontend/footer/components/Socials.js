a95dc901341583eabb197894b5f6e7bf526ca1c8
jaykaranja <mainakaranja2001@gmail.com>
2023-05-23 12:09:56 +0300

    Fixed responsiveness issues and added new illustration

--- src/components/frontend/footer/components/Socials.js
+++ src/components/frontend/footer/components/Socials.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faTwitter, faLinkedin  } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faGooglePlusG, faTwitter, faLinkedin  } from '@fortawesome/free-brands-svg-icons';
 
 export const Socials = () => {
   return (
       <div className='socials-icons'>
           <FontAwesomeIcon icon={faFacebook} className='social-icon' />
           <FontAwesomeIcon icon={faTwitter} className='social-icon' />
-          <FontAwesomeIcon icon={faInstagram} className='social-icon' />
+          <FontAwesomeIcon icon={faGooglePlusG} className='social-icon' />
           <FontAwesomeIcon icon={faLinkedin} className='social-icon' />
       </div>
   )
