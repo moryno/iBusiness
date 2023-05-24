e5b39d41d137b07c7c7909409562a2036a5fe467
moryno <mauricenganga41@gmail.com>
2023-05-24 16:41:14 +0300

    Add onboarding page to the frontend website

---
+++ src/helpers/onBoardingSource.js
@@ -0,0 +1,16 @@
+export const organizationCategoryOptions = [
+  "Automotive, Mobility & Transportation",
+  "Energy & Sustainability",
+  "Finance Services",
+  "Healthcare & Life Sciences",
+  "Manufacturing & Supply Chain",
+  "Media & Communications",
+  "Public Sector",
+  "Retail & Consumer Goods",
+  "Software",
+];
+export const servicePlanOptions = [
+  "Free (Free Forever)",
+  "Basic (Ksh3,000, Free 7 Days Trial)",
+  "Standard (Ksh10,000, Free 14 Days Trial)",
+];
