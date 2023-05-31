70a90b65e237f581546201719fe7bd78f0866711
James <mainakaranja2001@gmail.com>
2023-05-31 11:37:51 +0300

    UI tweaks

--- src/components/frontend/pricing/pricing-item/PricingItem.js
+++ src/components/frontend/pricing/pricing-item/PricingItem.js
@@ -5,9 +5,9 @@ import { msSingleSign } from "../../../../utils/webService";
 
 const PricingItem = ({ pricing }) => {
   return (
-    <main className="w-full px-5 box-border transition-all duration-[0.1s] md:w-1/2 lg:w-1/3 xl:w-1/4 mt-4 md:hover:-translate-y-1">
+    <main className="w-full px-5 box-border md:w-1/2 lg:w-1/3 xl:w-1/4 mt-4">
       <section>
-        <article className="py-3 px-5  bg-white rounded-lg shadow-2xl hover:shadow-3xl box-border">
+        <article className="py-3 px-5  bg-white rounded-lg shadow-2xl transition-all duration-[0.2s] hover:shadow-3xl box-border hover:-translate-y-1">
           <h4 className="font-semibold capitalize text-gray-400 text-center text-xl m-3">
             {pricing.title}
           </h4>
